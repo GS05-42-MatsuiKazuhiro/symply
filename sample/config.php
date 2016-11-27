c7bc03636a13d929a43a19136ca1a473683d63fd
Matsui Kazuhiro <kazuhiro.matsui@gmail.com>
2016-11-27 19:13:07 +0900

    config削除＆iPhone6対応

--- sample/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("Facebook/autoload.php");
- 
-$fb = new Facebook\Facebook([
-  'app_id' => '633743700084205',
-  'app_secret' => '3dd7825ae47666820be7ecbbeb903816',
-  'default_graph_version' => 'v2.5',
-]);
-?>
